2b6c18adecc6cdae9425d5ab2c845304b4eefdea
DanaJessica <dana@o3.network>
2022-08-11 14:27:14 +0800

    feat: add two buttons

--- src/app/carousel/test/test.component.ts
+++ src/app/carousel/test/test.component.ts
@@ -4,40 +4,69 @@ import { CAROUSEL_DATA } from 'src/app/data';
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
-  styleUrls: ['./test.component.scss']
+  styleUrls: ['./test.component.scss'],
 })
 export class TestComponent implements OnInit {
   carouselData = CAROUSEL_DATA;
-  curIndex:number = 0
+  curIndex: number = 0;
+  timer: any = null;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    this.autoPlay()
+    this.autoPlay();
+    this.stopOrStart();
   }
 
-  right(){
+  right() {
     const box = document.querySelector('ul') as unknown as HTMLParagraphElement;
     this.curIndex++;
-    if(this.curIndex===this.carouselData.length-1){
-      setTimeout(()=>{
+    if (this.curIndex === this.carouselData.length - 1) {
+      setTimeout(() => {
         box.style.transition = 'none';
         this.curIndex = 0;
-        box.style.left = `${- this.curIndex * 800}px`
-      },1000)
+        box.style.left = `${-this.curIndex * 800}px`;
+      }, 1000);
     }
-    box.style.transition = '1s ease'
-    box.style.left = `${- this.curIndex * 800}px`
+    box.style.transition = '1s ease';
+    box.style.left = `${-this.curIndex * 800}px`;
   }
 
-  autoPlay(){
-    setInterval(()=>{
-      this.right()      
-    },3000)
+  left() {
+    const box = document.querySelector('ul') as unknown as HTMLParagraphElement;
+    if (this.curIndex === 0) {
+      this.curIndex = this.carouselData.length - 1;
+      box.style.transition = 'none';
+      box.style.left = `${-this.curIndex * 800}px`;
+      setTimeout(() => {
+        this.curIndex = this.carouselData.length - 2;
+        box.style.transition = '1s ease';
+        box.style.left = `${-this.curIndex * 800}px`;
+      });
+    } else {
+      this.curIndex--;
+      box.style.transition = '1s ease';
+      box.style.left = `${-this.curIndex * 800}px`;
+    }
   }
 
-  clickCarousel(){
-    window.open(this.carouselData[this.curIndex].url)
+  autoPlay() {
+    this.timer = setInterval(() => {
+      this.right();
+    }, 3000);
+  }
+
+  stopOrStart() {
+    const box = document.querySelector('.carouselBox') as HTMLParagraphElement;
+    box.onmouseenter = () => {
+      clearInterval(this.timer);
+    };
+    box.onmouseleave = () => {
+      this.autoPlay();
+    };
+  }
+
+  clickCarousel() {
+    window.open(this.carouselData[this.curIndex].url);
   }
 }
-
